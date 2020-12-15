a605aa72a776af5388f6014ee3a0e2efdb1ca621
SLaxum <55930519+SLaxum@users.noreply.github.com>
2020-12-14 19:13:24 -0500

    Script IAEnemy + Health
    Script para la vida del jugador y Script para la IA basica de los enemigos

---
+++ Assets/IA Enemy/Health.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Health : MonoBehaviour
+{
+    public Text health;
+    public int healths = 1000;
+
+    // Update is called once per frame
+    void Update()
+    {
+        health.text = "VIDA: 1000/" + healths;
+
+        if(healths < 1)
+        {
+            health.text = "ESTAS MUERTO...";
+        }
+
+    }
+}
