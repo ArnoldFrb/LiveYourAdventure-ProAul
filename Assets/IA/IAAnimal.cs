781e3b5ae950f0184fa51c2147162f70dac4bfa1
SLaxum <55930519+SLaxum@users.noreply.github.com>
2020-12-17 13:33:57 -0500

    Mascots y Animales
    Se agrego una mascota que sigue al  jugador y gallinas al pueblo

---
+++ Assets/IA/IAAnimal.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IAAnimal : MonoBehaviour
+{
+    [Header("Settings IA Animals")]
+    public int time;
+    public bool timeOfTurn;
+    public float y;
+    public float speed;
+
+    // Update is called once per frame
+    void Update()
+    {
+        //IA PASSIVE
+        time += 1;
+        transform.Translate(transform.forward * speed * Time.fixedDeltaTime);
+        transform.Rotate(new Vector3(0, y, 0));
+
+        if (time >= Random.Range(100, 2500))
+        {
+            Turn();
+            time = 0;
+            timeOfTurn = true;
+        }
+
+        if (timeOfTurn)
+        {
+            if (time >= Random.Range(10, 30))
+            {
+                y = 0;
+                timeOfTurn = false;
+            }
+        }
+    }
+
+    //IA PASSIVE
+    public void Turn()
+    {
+        y = Random.Range(-3, 3);
+    }
+}
---
+++ Assets/IA/IAPet.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class IAPet : MonoBehaviour
+{
+    [Header("Settings IA Pet")]
+    public GameObject Target;
+    public NavMeshAgent agent;
+    public Animator Anim;
+    public float speed;
+    public float distance;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Vector3.Distance(Target.transform.position, transform.position) < distance)
+        {
+            agent.SetDestination(Target.transform.position);
+            agent.speed = speed;
+            
+        }
+        else
+        {
+            agent.speed = 0;
+        }
+
+        if (Vector3.Distance(Target.transform.position, transform.position) > agent.stoppingDistance + 0.3)
+        {
+            RunTrue();
+        }
+        else
+        {
+            RunFalse();
+            agent.speed = 0;
+        }
+    }
+
+    void RunTrue()
+    {
+        Anim.SetBool("Run", true);
+    }
+
+    void RunFalse()
+    {
+        Anim.SetBool("Run", false);
+    }
+}
