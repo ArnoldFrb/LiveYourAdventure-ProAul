1af4b4b7967bfa43d0d7caa47fcf20769750eaf0
SLaxum <55930519+SLaxum@users.noreply.github.com>
2020-12-18 17:57:00 -0500

    Barra de vida para enemigos

---
+++ Assets/IA/HealthEnemy.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthEnemy : MonoBehaviour
+{
+    [Header("Settings Health")]
+    public Image Ihealth;
+    public float healths;
+    public GameObject Enemy;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Ihealth.fillAmount = healths;
+
+        if (healths < 0.0001)
+        {
+            Enemy.gameObject.SetActive(false);
+        }
+    }
+}
